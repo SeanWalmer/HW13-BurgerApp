d8eaf54600c7d7cd52038db280e701fda635184c
SeanW <sean.walmer@outlook.com>
2020-06-16 12:26:37 -0400

    changed to horoku database settings

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,19 @@ var mysql = require("mysql");
 var databaseInfo = require("./databaseObj");
 
 // create the connection information for the sql database
-var connection = mysql.createConnection(databaseInfo);
+var connection = mysql.createConnection({
+    host: "dyud5fa2qycz1o3v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "yl1suaubi7atczxy",
+  
+    // Your password
+    password: "xf3kahvqo235rlwt",
+    database: "skn5zjgnkmk9b2fy"
+});
 
 connection.connect(function(err) {
   if (err) throw err;
